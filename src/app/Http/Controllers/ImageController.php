1b2b889de661f7077531b65f8bc27ee4e65ceec3
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-09 10:08:43 +0300

    c

---
+++ src/app/Http/Controllers/ImageController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Image;
+use Illuminate\Support\Facades\Storage;
+
+class ImageController extends Controller
+{
+    public function store(Request $request)
+    {
+
+        $token = '1';
+        // $token = $request->bearerToken();
+        // dd($request->file('image'));
+        // dd($request);
+
+
+        if ($token == '1' || $token == '4') {
+            $path = $request->file('image')->store('team', 's3');
+
+            // dd(basename($path));
+
+            //        Или ткут паблик делать, или в конфигах s3 добавить 'visibility' => 'public', или на самом aws
+            // Storage::disk('s3')->setVisibility($path, 'public');
+
+            // $image = Image::create([
+            //     'project_id' => $token,
+            //     // 'project_id' => $request->bearerToken(),
+            //     'filename' => basename($path),
+            //     'url' => Storage::disk('s3')->url($path),
+            //     'tagable_id' => $request->file('image')->getClientOriginalName()
+            // ]);
+
+            return basename($path);
+        }
+
+        // if ($token == '2') {
+
+        //     $path = $request->file('image')->store('luba-mebel/products', 's3');
+        //     Storage::disk('s3')->setVisibility($path, 'public');
+
+        //     $image = Image::create([
+        //         'project_id' => $request->bearerToken(),
+        //         'filename' => basename($path),
+        //         'url' => Storage::disk('s3')->url($path),
+        //         'tagable_id' => $request->file('image')->getClientOriginalName()
+        //     ]);
+
+        //     return $image;
+        // }
+
+        // if ($token == '3') {
+        //     $path = $request->file('image')->store('mebel-mobile/products', 's3');
+
+        //     Storage::disk('s3')->setVisibility($path, 'public');
+
+        //     $image = Image::create([
+        //         'project_id' => $request->bearerToken(),
+        //         'filename' => basename($path),
+        //         'url' => Storage::disk('s3')->url($path),
+        //         'tagable_id' => $request->file('image')->getClientOriginalName()
+        //     ]);
+
+        //     return $image;
+        // }
+    }
+
+    public function show(Request $request, Image $image) //TODO этот метод вообще нужен?
+    {
+        $token = $request->bearerToken();
+
+        if ($token == '1') {
+            return Storage::disk('s3')->response('images/' . $image->filename);
+        }
+
+        if ($token == '2') {
+            return Storage::disk('s3')->response('luba-mebel/products' . $image->filename);
+        }
+
+        if ($token == '3') {
+            return Storage::disk('s3')->response('mebel-mobile/products' . $image->filename);
+        }
+    }
+
+    public function delete(Request $request, $param)
+    {
+        Image::where('project_id', $request->bearerToken())
+            ->where('tagable_id', $param)
+            ->delete();
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ImageController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/health', function () {
+    return 'Hello World';
+});
+
+
+Route::post('/upload-image', [ImageController::class, 'store']);
+Route::get('/get-image/{image}', [ImageController::class, 'show']);
+Route::delete('/delete-image/{param}', [ImageController::class, 'delete']);
