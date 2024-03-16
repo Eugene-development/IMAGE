dbad9b4d1df021770c5870f16f97fb012553065b
Eugene Che <indexpro24@gmail.com>
2024-03-16 14:34:16 +0300

    c

--- src/app/Http/Controllers/ImageController.php
+++ src/app/Http/Controllers/ImageController.php
@@ -8,27 +8,46 @@ use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
+    // public function store(Request $request)
+    // {
+
+    //     // Определение проекта из заголовка
+    //     $projectHeaderValue = $request->header('Project');
+
+    //     // Назначение пути до целевой папки в бакете, в соответствии с именем бакета
+    //     $path = $request->file('image')->store($projectHeaderValue, 'yandex');
+
+    //     // Запись в бакет с публичными правами
+    //     Storage::disk('yandex')->setVisibility($path, 'public');
+
+    //     // Возврат хэша файла для записи в БД
+    //     return basename($path);
+    // }
+
+
     public function store(Request $request)
     {
+        // Проверка наличия файла 'image' и заголовка 'Project'
+        if (!$request->hasFile('image') || !$request->header('Project')) {
+            // Возвращаем ошибку, если что-то отсутствует
+            return response()->json(['error' => 'Необходим файл и заголовок Project'], 400);
+        }
 
-        // Определение проекта из заголовка
-        $projectHeaderValue = $request->header('Project');
+        try {
+            // Определение проекта из заголовка
+            $projectHeaderValue = $request->header('Project');
 
-        // Назначение пути до целевой папки в бакете, в соответствии с именем бакета
-        $path = $request->file('image')->store($projectHeaderValue, 'yandex');
+            // Назначение пути до целевой папки в бакете
+            $path = $request->file('image')->store($projectHeaderValue, config('filesystems.default'));
 
-        // Запись в бакет с публичными правами
-        Storage::disk('yandex')->setVisibility($path, 'public');
+            // Запись в бакет с публичными правами
+            Storage::disk(config('filesystems.default'))->setVisibility($path, 'public');
 
-        // Возврат хэша файла для записи в БД
-        return basename($path);
+            // Возврат хэша файла для записи в БД
+            return basename($path);
+        } catch (\Exception $e) {
+            // Обработка исключений, например, проблем с хранилищем
+            return response()->json(['error' => 'Ошибка при сохранении файла'], 500);
+        }
     }
-
-
-    // public function delete(Request $request, $param)
-    // {
-    //     Image::where('project_id', $request->bearerToken())
-    //         ->where('tagable_id', $param)
-    //         ->delete();
-    // }
 }
